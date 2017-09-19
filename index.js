029dd659a26f05cdc7f0cde2342a68ce3707f675
Ajaalexandra <ajacederberg@gmail.com>
2017-09-19 14:45:51 -0500

    Committed Chatty: Day 1 Project

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+// remember to have required packages at the beginning, then middle wear in the center, and app.listen @ the end 
+const express = require('express');
+const bodyParser = require('body-parser');
+var app = express();
 
+
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+var messages =[];
+
+
+app.get('/messages', function (req, res, next) {
+  res.status(200).json({ messages: messages });
+});
+
+app.post('/messages', function (req, res, next) {
+  messages.push({ message: req.body.message, time: new Date() });
+  res.status(200).json({ messages: messages });
+});
+
+
+
+
+app.listen(3000, () => {
+  console.log(`Listening on 3000`)
+});
